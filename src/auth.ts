cb838b35f2487273c156af2d021cdf4f93a026fc
Harish Kumar <harishkumarjohnson@gmail.com>
2026-07-05 18:31:26 +0530

    fix:google auth

--- src/auth.ts
+++ src/auth.ts
@@ -20,10 +20,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     Credentials({
       name: "Credentials",
